51efcfd75a5428e8cd18f4cfddb7ef4b8b2fb3de
Bharti277 <bhartideependra277@gmail.com>
2025-05-23 20:30:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      name,
+      age,
+      email,
+    };
+    fetch("http://localhost:4000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData([...data, user]);
+        setName("");
+        setAge(0);
+        setEmail("");
+      });
+  };
+  return (
+    <>
+      <div className="app">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              name="name"
+              id="name"
+              placeholder="Enter Note"
+              required
+            />
+            <input
+              type="number"
+              value={age}
+              min={0}
+              onChange={(e) => setAge(e.target.value)}
+              name="age"
+              id="age"
+              placeholder="Enter Age"
+              required
+            />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              id="email"
+              placeholder="Enter Email"
+              required
+            />
+            <button>Add New Note</button>
+          </div>
+        </form>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.age}</td>
+                <td>{user.email}</td>
+                <td>Edit</td>
+                <td>Delete</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
